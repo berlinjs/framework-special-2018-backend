6f92477717ec26453643e4faa00f6a59d8bbdfc3
Dominik Kundel <dominik.kundel@gmail.com>
2017-12-03 12:35:33 +0100

    :sparkles: Enable CORS

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ const PORT = process.env.PORT || 3000;
 let db;
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 app.use(cookieParser());
 app.use((req, res, next) => {
   req.db = db;
