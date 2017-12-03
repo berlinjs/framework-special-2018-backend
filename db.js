ccdb12ac47c6650fc8896183c395eab5f8185f54
Dominik Kundel <dominik.kundel@gmail.com>
2017-12-03 12:34:36 +0100

    :card_file_box: Add additional meetup entries

--- db.js
+++ db.js
@@ -12,11 +12,55 @@ const MEETUP_ENTRIES = [
       latitude: '52.500330',
       longitude: '13.419786'
     },
-    description: 'This is some description',
+    description:
+      'Berlin.js is a montly meetup for everything around JavaScript',
     time: '19:00',
     day: 'Every 3rd Thursday',
     nextMeetup: new Date(),
-    twitter: 'berlinjs'
+    twitter: 'berlinjs',
+    url: 'https://www.meetup.com/Berlin-JS/'
+  },
+  {
+    name: 'Angular Berlin',
+    location: 'bitcrowd GmbH',
+    coordinates: {
+      latitude: '52.499892',
+      longitude: '13.425056'
+    },
+    description: 'A monthly meetup around AngularJS & Angular',
+    time: '19:00',
+    day: 'Every 2nd Wednesday',
+    nextMeetup: new Date(),
+    twitter: 'angular_berlin',
+    url: 'https://www.meetup.com/AngularJS-Meetup-Berlin/'
+  },
+  {
+    name: 'Ionic Berlin',
+    location: 'JustWatch GmbH',
+    coordinates: {
+      latitude: '52.512911',
+      longitude: '13.455111'
+    },
+    description: 'A quarterly meetup around Ionic & Stencil',
+    time: '19:00',
+    day: 'Irregularly Thursdays',
+    nextMeetup: new Date(),
+    twitter: 'IonicBerlin',
+    url: 'https://www.meetup.com/Ionic-Berlin/'
+  },
+  {
+    name: 'Ember Berlin',
+    location: 'kloeckner.i',
+    coordinates: {
+      latitude: '52.531530',
+      longitude: '13.383500'
+    },
+    description: 'A monthly meetup around Ember & Glimmer',
+    time: '19:00',
+    day: 'First Tuesday',
+    nextMeetup: new Date(),
+    twitter: 'emberliners',
+    url: 'https://www.meetup.com/Ember-js-Berlin/'
   }
 ].map(addIds);
 
