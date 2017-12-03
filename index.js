5dab66a66cf8b4d7bf4069c8b2ccb3560fd756ad
Dominik Kundel <dominik.kundel@gmail.com>
2017-12-03 16:46:13 +0100

    :bug: Fix CORS headers to allow authorizaiton

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
   next();
 });
